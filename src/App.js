6ba53c6ab04bb19a714723a0f1473e73cc62ecb5
baige369 <89885506+baige369@users.noreply.github.com>
2022-03-17 07:24:14 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <p>Hello</p>
+      <a href="#" className="btn btn-primary">
+        Push the button
+      </a>
+    </div>
+  );
+}
