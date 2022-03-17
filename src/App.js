f11c625a512cc3a16085cb171f69d1d63f2a5f36
baige369 <89885506+baige369@users.noreply.github.com>
2022-03-17 17:35:39 +0000

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <p>Hello</p>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            alt="SheCodes logo"
+            className="App-logo img-fluid"
+          ></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Ewelina Baig</small>
+        </footer>
+      </div>
     </div>
   );
 }
